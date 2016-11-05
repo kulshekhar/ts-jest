2f28009e114f84954096f00933ef126274dc355e
kulshekhar <kulshekhar@users.noreply.github.com>
2016-11-06 03:18:25 +0530

    use SIGTERM

--- tests/__tests__/watch.spec.ts
+++ tests/__tests__/watch.spec.ts
@@ -54,36 +54,35 @@ describe('hello_world', () => {
     result = runJestInWatchMode('../watch-test');
     DEFAULT_TIMEOUT_INTERVAL = jasmine['DEFAULT_TIMEOUT_INTERVAL'];
     jasmine['DEFAULT_TIMEOUT_INTERVAL'] = 10000;
-    console.log('>>>>>>>', process.platform);
   });
 
-  it.skip('should show the correct error locations in the typescript files without changes', () => {
+  it('should show the correct error locations in the typescript files without changes', () => {
     return result.getStderrAsync().then((stderr) => {
       expect(stderr).toContain('Hello.ts:13:11');
       expect(stderr).toContain('Hello.test.ts:9:19');
-    });
+    }).catch((e) => { });
   });
 
-  it.skip('should show the correct error locations in the typescript files with changes in source file', () => {
+  it('should show the correct error locations in the typescript files with changes in source file', () => {
     fs.writeFileSync(path.resolve(__dirname, '../watch-test/Hello.ts'), helloFileUpdate);
     let promise = result.getStderrAsync().then((stderr) => {
       expect(stderr).toContain('Hello.ts:11:11');
       expect(stderr).toContain('Hello.test.ts:9:19');
-    })
+    }).catch((e) => { });
     return promise;
   });
 
   it('should show the correct error locations in the typescript files with changes in source file and test file', () => {
+    fs.writeFileSync(path.resolve(__dirname, '../watch-test/__tests__/Hello.test.ts'), testFileUpdate);
     let promise = result.getStderrAsync().then((stderr) => {
       expect(stderr).toContain('Hello.ts:11:11');
       expect(stderr).toContain('Hello.test.ts:11:19');
-    });
-    fs.writeFileSync(path.resolve(__dirname, '../watch-test/__tests__/Hello.test.ts'), testFileUpdate);
+    }).catch((e) => { });
     return promise;
   });
 
   afterAll(() => {
-    result.childProcess.kill();
+    result.childProcess.kill('SIGTERM');
     // revert changes back
     jasmine['DEFAULT_TIMEOUT_INTERVAL'] = DEFAULT_TIMEOUT_INTERVAL;
     fs.writeFileSync(path.resolve(__dirname, '../watch-test/Hello.ts'), helloFile);
