49518eb2ff292cfa6c127418400b54b6c23562c6
Igor Chulinda <ichulinda@dataart.com>
2016-12-01 16:40:31 +0100

    Temporary fix for #53 omitting some tests on AppVeyor.

--- scripts/tests.js
+++ scripts/tests.js
@@ -8,7 +8,13 @@ const argv = process.argv.slice(2);
 argv.push('--no-cache');
 // Watch unless on CI
 if (!process.env.CI) {
-  //argv.push('--watch');
+  // argv.push('--watch');
+}
+// omit tests for watch cases if it runned on AppVeyor due to this issues:
+// https://github.com/kulshekhar/ts-jest/issues/53
+// http://help.appveyor.com/discussions/problems/5500-nodejs-child-process-with-watch-and-stderr-problem
+if (process.env.AppVeyor) {
+  argv.push('--testPathPattern', '^(?!(.*watch\.spec\.ts$)).*');
 }
 
 jest.run(argv);
