649c2fc4a5c9f4e6848a8677ba3d566f90cd8fda
kulshekhar <kulshekhar@users.noreply.github.com>
2016-12-22 22:03:13 +0530

    Disable tests for watch mode

--- scripts/tests.js
+++ scripts/tests.js
@@ -8,17 +8,17 @@ const fs = require('fs');
 const path = require('path');
 
 function dirExists(dirPath) {
-  const F_OK = fs.constants && fs.constants.F_OK || fs['F_OK'];
-  try {
-    fs.accessSync(dirPath, F_OK);
-    return fs.statSync(dirPath).isDirectory();
-  } catch (e) {
-    return false;
-  }
+    const F_OK = fs.constants && fs.constants.F_OK || fs['F_OK'];
+    try {
+        fs.accessSync(dirPath, F_OK);
+        return fs.statSync(dirPath).isDirectory();
+    } catch (e) {
+        return false;
+    }
 }
 
 function getDirectories(rootDir) {
-    return fs.readdirSync(rootDir).filter(function(file) {
+    return fs.readdirSync(rootDir).filter(function (file) {
         return fs.statSync(path.join(rootDir, file)).isDirectory();
     });
 }
@@ -26,14 +26,14 @@ function getDirectories(rootDir) {
 function createIntegrationMock() {
     const integrationMockContent = 'module.exports = require(\'../../../\');';
     const testsRoot = 'tests';
-    const testCaseFolders = getDirectories(testsRoot).filter(function(testDir) {
+    const testCaseFolders = getDirectories(testsRoot).filter(function (testDir) {
         return !(testDir.startsWith('__') && testDir.endsWith('__'));
     });
     for (let i = 0; i < testCaseFolders.length; i++) {
         const testCaseModuleFolder = path.join(testsRoot, testCaseFolders[i], 'node_modules');
         if (!dirExists(testCaseModuleFolder)) {
             fs.mkdirSync(testCaseModuleFolder);
-            const integrationMockPath = path.join(testCaseModuleFolder, 'ts-jest.js');            
+            const integrationMockPath = path.join(testCaseModuleFolder, 'ts-jest.js');
             fs.appendFileSync(integrationMockPath, integrationMockContent);
         }
     }
@@ -50,8 +50,7 @@ if (!process.env.CI) {
 // omit tests for watch cases if it runned on AppVeyor due to this issues:
 // https://github.com/kulshekhar/ts-jest/issues/53
 // http://help.appveyor.com/discussions/problems/5500-nodejs-child-process-with-watch-and-stderr-problem
-if (process.env.AppVeyor) {
-    argv.push('--testPathPattern', '^(?!(.*watch\.spec\.ts$)).*');
-}
+
+argv.push('--testPathPattern', '^(?!(.*watch\.spec\.ts$)).*');
 
 jest.run(argv);
