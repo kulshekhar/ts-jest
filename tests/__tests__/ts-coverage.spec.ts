5b343a075dfb6f5511da410bd68bacd82b84a748
Henry Zektser <japhar81@gmail.com>
2017-03-17 17:42:03 -0400

    Bring back path.sep

--- tests/__tests__/ts-coverage.spec.ts
+++ tests/__tests__/ts-coverage.spec.ts
@@ -7,7 +7,7 @@ describe('hello_world', () => {
     `-----------|----------|----------|----------|----------|----------------|
 File       |  % Stmts | % Branch |  % Funcs |  % Lines |Uncovered Lines |
 -----------|----------|----------|----------|----------|----------------|
- simple/   |    90.91 |       50 |       80 |    88.89 |                |
+ simple${path.sep}   |    90.91 |       50 |       80 |    88.89 |                |
   Hello.ts |    90.91 |       50 |       80 |    88.89 |             20 |
 -----------|----------|----------|----------|----------|----------------|
 All files  |    90.91 |       50 |       80 |    88.89 |                |
@@ -20,8 +20,7 @@ All files  |    90.91 |       50 |       80 |    88.89 |                |
     const coveragePath = path.resolve('./tests/simple/coverage/remapped/coverage.txt');
 
     expect(fs.statSync(coveragePath).isFile()).toBeTruthy();
-    // TODO: Turning this off -- appveyor formats differently and blows up
-    // expect(fs.readFileSync(coveragePath, 'utf-8')).toEqual(snapshot);
+    expect(fs.readFileSync(coveragePath, 'utf-8')).toEqual(snapshot);
   });
 
 });
