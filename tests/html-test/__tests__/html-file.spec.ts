72e0e56b4d7da6829362bc6d2c6946e571a91497
Michał Pierzchała <thymikee@gmail.com>
2017-03-27 15:34:14 +0200

    Update test

--- tests/html-test/__tests__/html-file.spec.ts
+++ tests/html-test/__tests__/html-file.spec.ts
@@ -1,12 +1,14 @@
 import {process} from '../../../preprocessor';
 const config = require('../package.json');
 
-test('html to be transformed', () => {
+test('transforms html if config.globals.__TRANSFORM_HTML__ is set', () => {
   const source =
 `<div class="html-test">
   <span class="html-test__element">This is element</span>
 </div>`;
   const path = '/path/to/file.html';
 
+  expect(process(source, path, config.jest)).toMatchSnapshot();
+  delete config.jest.globals.__TRANSFORM_HTML__;
   expect(process(source, path, config.jest)).toMatchSnapshot();
 });
