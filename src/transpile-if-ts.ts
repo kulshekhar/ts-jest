e3f43dbb5c4f553493a1e8eac2d28cc7ec178af8
Marshall Bowers <elliott.codes@gmail.com>
2017-04-18 16:35:13 -0400

    Import TypeScript from configurable location

--- src/transpile-if-ts.ts
+++ src/transpile-if-ts.ts
@@ -1,9 +1,9 @@
-import * as tsc from 'typescript';
-import { getTSConfig } from './utils';
+import { getTsc, getTSConfig } from './utils';
 
 export function transpileIfTypescript(path, contents, config?) {
   if (path && (path.endsWith('.tsx') || path.endsWith('.ts'))) {
 
+    const tsc = getTsc(config && config.globals);
     let transpiled = tsc.transpileModule(contents, {
       compilerOptions: getTSConfig(config || { __TS_CONFIG__: global['__TS_CONFIG__'] }, true),
       fileName: path
