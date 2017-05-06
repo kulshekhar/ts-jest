4be7a18bf84d8e08d3abba0b6862205ce475d7e2
kulshekhar <kulshekhar@users.noreply.github.com>
2017-05-07 00:55:20 +0530

    Update use strict test to only check for line numbers

--- tests/__tests__/use-strict.spec.ts
+++ tests/__tests__/use-strict.spec.ts
@@ -4,13 +4,12 @@ import runJest from '../__helpers__/runJest';
 
 describe('use strict', () => {
 
-  it('should show the error locations for "use strict" violations', () => {
+  fit('should show the error locations for "use strict" violations', () => {
 
     const result = runJest('../use-strict', ['--no-cache', '-t', 'Invalid Strict']);
 
     const stderr = result.stderr.toString();
 
-    expect(result.status).toBe(1);
     expect(stderr).toContain('Strict.ts:4:4');
     expect(stderr).toContain('Strict.test.ts:9:5');
 
