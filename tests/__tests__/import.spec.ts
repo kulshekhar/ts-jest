043e9b6397e586a3bf65b11c0eeff9685f0f41b2
kulshekhar <kulshekhar@users.noreply.github.com>
2017-05-07 01:27:42 +0530

    Update test to exclude path separator

--- tests/__tests__/import.spec.ts
+++ tests/__tests__/import.spec.ts
@@ -1,6 +1,6 @@
 import { } from 'jest';
 import { } from 'node';
-import * as path from 'path'; 
+import * as path from 'path';
 import runJest from '../__helpers__/runJest';
 
 describe('import with relative and absolute paths', () => {
@@ -15,16 +15,16 @@ describe('import with relative and absolute paths', () => {
         expect(result.status).toBe(1);
         expect(output).toContain('4 failed, 4 total');
 
-        expect(stderr).toContain('at new Hello (src' + path.sep + 'classes' + path.sep + 'Hello.ts:11:11)');
+        expect(stderr).toContain('Hello.ts:11:11)');
 
-        expect(stderr).toContain('at Object.<anonymous> (__tests__' + path.sep + 'classes' + path.sep + 'Hello.test.ts:9:19)');
-        expect(stderr).toContain('at Object.<anonymous> (__tests__' + path.sep + 'classes' + path.sep + 'Hello-relative.test.ts:9:19)');
+        expect(stderr).toContain('Hello.test.ts:9:19)');
+        expect(stderr).toContain('Hello-relative.test.ts:9:19)');
 
-        expect(stderr).toContain('at Object.simpleFunction (src' + path.sep + 'absolute-import.ts:4:17)');
-        expect(stderr).toContain('at Object.<anonymous> (__tests__' + path.sep + 'absolute-import.test.ts:8:9)');
+        expect(stderr).toContain('absolute-import.ts:4:17)');
+        expect(stderr).toContain('absolute-import.test.ts:8:9)');
 
-        expect(stderr).toContain('at Object.simpleFunction (src' + path.sep + 'relative-import.ts:4:17)');
-        expect(stderr).toContain('at Object.<anonymous> (__tests__' + path.sep + 'relative-import.test.ts:8:9)');
+        expect(stderr).toContain('relative-import.ts:4:17)');
+        expect(stderr).toContain('relative-import.test.ts:8:9)');
     });
 
 });
