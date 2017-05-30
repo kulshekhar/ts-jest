5de92d06317820ecfb2b495ffe65f5aaa312a413
Gustav Wengel <gustavwengel@gmail.com>
2017-05-30 12:59:53 +0200

    new babel regression test

--- tests/__tests__/regression-tests.spec.ts
+++ tests/__tests__/regression-tests.spec.ts
@@ -22,7 +22,24 @@ const allowSyntheticDefaultImportsConfig = {
 	}
 };
 
+const skipBabelConfig = {
+	cacheDirectory:
+		'/tmp/cacheStuff',
+	globals: {
+		__TS_CONFIG__: {
+			allowSyntheticDefaultImports: true,
+			jsx: 'react',
+		},
+		'ts-jest':
+			{
+				skipBabel: true,
+			}
+	}
+};
+
 describe('Process regession tests', () => {
+	//These tests are to make that we only intentionally change the output of process.
+	
 	it('Should not change the output of process that compiles .ts files', () => {
 		const src = fs.readFileSync('./tests/simple/Hello.ts', 'utf-8');
 		const path = './tests/simple/Hello.ts';
@@ -56,5 +73,16 @@ describe('Process regession tests', () => {
 		expect(outcome).toMatchSnapshot();
 	});
 	
+	it('Should not change the output of process that contains hoist calls', () => {
+		//TODO
+	});
+	
+	it('Should not change the output of a process that has skipBabel set to true', () => {
+		const src = fs.readFileSync('./tests/synthetic-default/__tests__/module.test.ts', 'utf-8');
+		const path = './tests/synthetic-default/__tests__/module.test.ts';
+		const outcome = process(src, path, skipBabelConfig);
+		
+		expect(outcome).toMatchSnapshot();
+	});
 	
 });
