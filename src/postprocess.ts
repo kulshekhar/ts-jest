87630d81b6b26e83a0af70cf45b447408f176d7d
Gustav Wengel <gustavwengel@gmail.com>
2017-05-31 11:46:33 +0200

    maybe two spaces is right

--- src/postprocess.ts
+++ src/postprocess.ts
@@ -8,52 +8,52 @@ import * as babel from 'babel-core';
 import {CompilerOptions} from 'typescript/lib/typescript';
 
 function createBabelTransformer(options: PostProcessorOptions) {
-    options = {
-        ...options,
-        plugins: (options && options.plugins) || [],
-        presets: ((options && options.presets) || []).concat([jestPreset]),
-        retainLines: true,
-    };
-    delete options.cacheDirectory;
-    delete options.filename;
+  options = {
+    ...options,
+    plugins: (options && options.plugins) || [],
+    presets: ((options && options.presets) || []).concat([jestPreset]),
+    retainLines: true,
+  };
+  delete options.cacheDirectory;
+  delete options.filename;
 
-    return (src: string,
-            filename: string,
-            config: JestConfig,
-            transformOptions: TransformOptions): string => {
-        const theseOptions = Object.assign({filename}, options);
-        if (transformOptions && transformOptions.instrument) {
-            theseOptions.auxiliaryCommentBefore = ' istanbul ignore next ';
-            // Copied from jest-runtime transform.js
-            theseOptions.plugins = theseOptions.plugins.concat([
-                [
-                    require('babel-plugin-istanbul').default,
-                    {
-                        // files outside `cwd` will not be instrumented
-                        cwd: config.rootDir,
-                        exclude: [],
-                    },
-                ],
-            ]);
-        }
-        return babel.transform(src, theseOptions).code;
-    };
+  return (src: string,
+          filename: string,
+          config: JestConfig,
+          transformOptions: TransformOptions): string => {
+    const theseOptions = Object.assign({filename}, options);
+    if (transformOptions && transformOptions.instrument) {
+      theseOptions.auxiliaryCommentBefore = ' istanbul ignore next ';
+      // Copied from jest-runtime transform.js
+      theseOptions.plugins = theseOptions.plugins.concat([
+        [
+          require('babel-plugin-istanbul').default,
+          {
+            // files outside `cwd` will not be instrumented
+            cwd: config.rootDir,
+            exclude: [],
+          },
+        ],
+      ]);
+    }
+    return babel.transform(src, theseOptions).code;
+  };
 }
 
 export const getPostProcessHook = (tsCompilerOptions: CompilerOptions, jestConfig: JestConfig, tsJestConfig: any): PostProcessHook => {
-    if (tsJestConfig.skipBabel) {
-        return (src) => src; //Identity function
-    }
+  if (tsJestConfig.skipBabel) {
+    return (src) => src; //Identity function
+  }
 
-    const plugins = [];
-    //If we're not skipping babel
-    if (tsCompilerOptions.allowSyntheticDefaultImports) {
-        plugins.push('transform-es2015-modules-commonjs');
-    }
+  const plugins = [];
+  //If we're not skipping babel
+  if (tsCompilerOptions.allowSyntheticDefaultImports) {
+    plugins.push('transform-es2015-modules-commonjs');
+  }
 
 
-    return createBabelTransformer({
-        presets: [],
-        plugins: plugins,
-    });
+  return createBabelTransformer({
+    presets: [],
+    plugins: plugins,
+  });
 };
