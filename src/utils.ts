1224b4d11a5fd688d2a5d0647fd1faaceea5a8f4
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-07-07 11:23:21 +0430

    Fix unnecessary if condition

--- src/utils.ts
+++ src/utils.ts
@@ -111,13 +111,12 @@ export function getTSConfigOptionFromConfig(globals: any) {
 
   const tsJestConfig = getTSJestConfig(globals);
 
-  if (('__TS_CONFIG__' in globals) && globals.__TS_CONFIG__) {
+  if (globals.__TS_CONFIG__) {
     console.warn(`Using globals > __TS_CONFIG__ option for setting TS config is deprecated. 
-
 Please set config using this option:\nglobals > ts-jest > tsConfig (object) or tsConfigFile 
 (string). More information at https://github.com/kulshekhar/ts-jest#tsconfig`);
     return globals.__TS_CONFIG__;
-  } else if ('tsConfigFile' in tsJestConfig && tsJestConfig.tsConfigFile) {
+  } else if (tsJestConfig.tsConfigFile) {
     return tsJestConfig.tsConfigFile;
   }
 
