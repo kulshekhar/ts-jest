66e88e7112ee781136315582b7c6b856e4ed6f34
Junle Li <lijunle@gmail.com>
2017-08-02 00:03:05 +0800

    Fix typo: commmonjs -> commonjs

--- src/utils.ts
+++ src/utils.ts
@@ -180,7 +180,7 @@ export function getTSConfig(globals, collectCoverage: boolean = false) {
     return config;
   } else {
     config.jsx = config.jsx || 'react';
-    config.module = config.module || 'commmonjs';
+    config.module = config.module || 'commonjs';
     if (config.allowSyntheticDefaultImports && !skipBabel) {
       // compile ts to es2015 and transform with babel afterwards
       config.module = 'es2015';
