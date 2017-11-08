f3ba44e02964c60dd674645474bcc68cc2b55d11
Kulshekhar Kabra <kulshekhar@gmail.com>
2017-11-08 13:37:33 +0530

    Fix line number in a test

--- tests/__tests__/tsx-errors.spec.ts
+++ tests/__tests__/tsx-errors.spec.ts
@@ -9,6 +9,6 @@ describe('TSX Errors', () => {
 
     expect(result.status).toBe(1);
     expect(stderr).toContain('Button.tsx:22:17');
-    expect(stderr).toContain('Button.test.tsx:16:12');
+    expect(stderr).toContain('Button.test.tsx:19:12');
   });
 });
