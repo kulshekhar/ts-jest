ec89ebab97a3b41213b929099d6434d6893915ca
Gustav Wengel <gustavwengel@gmail.com>
2017-12-15 22:00:43 +0100

    added comment

--- src/logger.ts
+++ src/logger.ts
@@ -2,6 +2,12 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { JestConfig } from './jest-types';
 
+/**
+ * Logger file that enables logging things just once. Does this by traversing the array of previously recorded
+ * logs to see if the exact same message has already been logged
+ * @type {any[]}
+ */
+
 const logs: any[] = [];
 let loggingDisabled: boolean = true;
 let logsFlushed: boolean = false;
@@ -29,6 +35,7 @@ export function logOnce(...thingsToLog: any[]) {
   });
 }
 
+// This function JSONifies logs and flushes them to disk.
 export function flushLogs() {
   if (loggingDisabled || logsFlushed) {
     return; // only output stuff for the first invocation and if logging is enabled.
@@ -38,7 +45,6 @@ export function flushLogs() {
   const JSONifiedLogs = logs.map(convertToJSONIfPossible);
   const logString = JSONifiedLogs.join('\n');
   const filePath = path.resolve(rootPath, 'debug.txt');
-  console.warn('Writing file to : ', filePath);
   fs.writeFileSync(filePath, logString);
 }
 
