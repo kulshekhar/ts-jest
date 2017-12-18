540ad1b7920dbaafcbf6147ebc4a71c2c3df53f7
kulshekhar <kulshekhar@users.noreply.github.com>
2017-12-18 18:27:26 +0530

    revert last change

--- scripts/tests.js
+++ scripts/tests.js
@@ -64,3 +64,17 @@ function createIntegrationMock() {
 }
 
 createIntegrationMock();
+
+const argv = process.argv.slice(2);
+argv.push('--no-cache');
+// Watch unless on CI
+if (!process.env.CI) {
+  // argv.push('--watch');
+}
+// omit tests for watch cases if it runned on AppVeyor due to this issues:
+// https://github.com/kulshekhar/ts-jest/issues/53
+// http://help.appveyor.com/discussions/problems/5500-nodejs-child-process-with-watch-and-stderr-problem
+
+argv.push('--testPathPattern', '^(?!(.*watch.spec.ts$)).*');
+
+jest.run(argv);
