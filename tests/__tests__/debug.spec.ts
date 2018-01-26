4348cb25367274502b996b890b30f8bcd0b4cb5a
Gustav Wengel <gustavwengel@gmail.com>
2018-01-26 18:52:43 +0100

    removed debug folder

--- tests/__tests__/debug.spec.ts
+++ tests/__tests__/debug.spec.ts
@@ -1,14 +1,23 @@
-import * as fs from 'fs';
+import * as fs from 'fs-extra';
 import * as path from 'path';
 import runJest from '../__helpers__/runJest';
 
+const debugFilePath = path.resolve(
+  __dirname,
+  '../simple/node_modules/ts-jest/debug.txt',
+);
+
 describe('Debug output', () => {
+  beforeEach(async () => {
+    return fs.remove(debugFilePath);
+  });
+
   it('should create a debug file with the correct output if the flag is set', async () => {
-    runJest('../debug', ['--no-cache', '-u'], {
+    runJest('../simple', ['--no-cache', '-u'], {
       TS_JEST_DEBUG: 'true',
     });
-    const logFile = fs.readFileSync(
-      path.resolve(__dirname, '../debug/node_modules/ts-jest/debug.txt'),
+    const logFile = await fs.readFile(
+      path.resolve(__dirname, '../simple/node_modules/ts-jest/debug.txt'),
       'utf-8',
     );
 
@@ -16,10 +25,10 @@ describe('Debug output', () => {
     expect(logFile).toMatchSnapshot(); // Ensure we have some actual output that doesn't change
   });
 
-  it('Should not create a file if the debug flag is not set', () => {
+  it('Should not create a file if the debug flag is not set', async () => {
     runJest('../simple', ['--no-cache', '-u']);
-    expect(() => {
-      fs.readFileSync(
+    expect(async () => {
+      await fs.readFile(
         path.resolve(__dirname, '../simple/node_modules/ts-jest/debug.txt'),
         'utf-8',
       );
--- tests/debug/__tests__/Hello.test.ts
+++
@@ -1,7 +0,0 @@
-import { Hello } from '../Hello';
-
-describe('Hello Class', () => {
-  it('should throw an error on line 18', () => {
-    const hello = new Hello();
-  });
-});
