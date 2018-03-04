17c68725467a951722327c7a6940e2c8fae06b35
Gustav Wengel <gustavwengel@gmail.com>
2018-03-04 21:09:36 +0100

    Long path (#461)
    * fixed long path issue #409

--- README.md
+++ README.md
@@ -389,8 +389,6 @@ npm install
 npm test
 ```
 
-**Note:** If you are cloning on Windows, you may have to run `git config --system core.longpaths true` for Windows to stop complaining about long filenames.
-
 ## License
 
 Copyright (c) [Authors](AUTHORS).
--- tests/__tests__/long-path.spec.ts
+++ tests/__tests__/long-path.spec.ts
@@ -1,6 +1,16 @@
 import runJest from '../__helpers__/runJest';
+import * as fs from 'fs-extra';
 
 describe('Long path', () => {
+  const longPath =
+    'long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/';
+  const destinationPath = `${__dirname}/../simple-long-path/${longPath}`;
+  beforeEach(async () => {
+    // root dir is project
+    await fs.ensureDir(destinationPath);
+    await fs.copy(`${__dirname}/../simple-long-path/src`, destinationPath);
+  });
+
   it('should work as expected', () => {
     const result = runJest('../simple-long-path/', [
       '--no-cache',
