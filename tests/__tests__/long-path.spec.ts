a25c01e44cae08bd7a0ac73ee6ad4a17a42aef9a
Gustav Wengel <gustavwengel@gmail.com>
2018-03-04 17:24:47 +0100

    little cleanup

--- tests/__tests__/long-path.spec.ts
+++ tests/__tests__/long-path.spec.ts
@@ -4,14 +4,11 @@ import * as fs from 'fs-extra';
 describe('Long path', () => {
   const longPath =
     'long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/';
+  const destinationPath = `./tests/simple-long-path/${longPath}`;
   beforeEach(async () => {
     // root dir is project
-    await fs.remove('./tests/simple-long-path/long-src-path');
-    await fs.ensureDir(`./tests/simple-long-path/${longPath}`);
-    await fs.copy(
-      './tests/simple-long-path/src',
-      `./tests/simple-long-path/${longPath}`,
-    );
+    await fs.ensureDir(destinationPath);
+    await fs.copy('./tests/simple-long-path/src', destinationPath);
   });
 
   it('should work as expected', () => {
