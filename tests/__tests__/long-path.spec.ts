4003edfa74da0e1dbecc0354ce0ce734b760d5a8
Gustav Wengel <gustavwengel@gmail.com>
2018-03-04 17:22:25 +0100

    fixed long path issue

--- tests/__tests__/long-path.spec.ts
+++ tests/__tests__/long-path.spec.ts
@@ -1,6 +1,19 @@
 import runJest from '../__helpers__/runJest';
+import * as fs from 'fs-extra';
 
 describe('Long path', () => {
+  const longPath =
+    'long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/long-src-path/';
+  beforeEach(async () => {
+    // root dir is project
+    await fs.remove('./tests/simple-long-path/long-src-path');
+    await fs.ensureDir(`./tests/simple-long-path/${longPath}`);
+    await fs.copy(
+      './tests/simple-long-path/src',
+      `./tests/simple-long-path/${longPath}`,
+    );
+  });
+
   it('should work as expected', () => {
     const result = runJest('../simple-long-path/', [
       '--no-cache',
