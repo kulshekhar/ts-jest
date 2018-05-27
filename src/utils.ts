ca09eede56a6e22ce5c0e3bbc6138ed78cccb249
Gustav Wengel <gustavwengel@gmail.com>
2018-05-27 18:41:44 +0200

    pushed line

--- src/utils.ts
+++ src/utils.ts
@@ -177,6 +177,7 @@ export function runTsDiagnostics(
   compilerOptions: tsc.CompilerOptions,
 ): void {
   const program = tsc.createProgram([filePath], compilerOptions);
+
   const allDiagnostics = tsc.getPreEmitDiagnostics(program);
 
   if (allDiagnostics.length) {
