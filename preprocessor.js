5368bb4ecd52285d7a0a64ca6b441ed76f9e2942
Kulshekhar Kabra <kulshekhar@gmail.com>
2018-08-03 13:56:24 +0530

    typo

--- preprocessor.js
+++ preprocessor.js
@@ -1,5 +1,5 @@
 console.warn(
-  `[ts-jest][DEPRECATED] - replace any occurrences to "ts-jest/dist/preprocessor.js" or ` +
+  `[ts-jest][DEPRECATED] - replace any occurrences of "ts-jest/dist/preprocessor.js" or ` +
     ` "<rootDir>/node_modules/ts-jest/preprocessor.js"` +
     ` in the 'transform' section of your Jest config with just "ts-jest".`
 );
