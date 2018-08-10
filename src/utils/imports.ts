ae68efdcb6873073b119130eab05bba1e17b0d3d
Chandler Prall <chandler.prall@gmail.com>
2018-08-10 12:26:43 -0600

    use json5 type for import return value

--- src/utils/imports.ts
+++ src/utils/imports.ts
@@ -3,6 +3,7 @@ import {
   TBabelPresetEnv,
   TBabelPluginIstanbul,
   TBabelPresetJest,
+  TJSON5,
 } from '../types';
 
 export function importBabelCore(): TBabel {
@@ -46,7 +47,7 @@ export function importBabelPluginIstanbul(): TBabelPluginIstanbul {
   return mod.default;
 }
 
-export function importJson5() {
+export function importJson5(): TJSON5 {
   const mod = tryRequire('json5');
   if (!mod) {
     throw new Error(
