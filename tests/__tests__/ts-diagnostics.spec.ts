5882591e288b37f752451ac27682dac075ddc3eb
Yaoyao Lin <yaoyao.lin@sap.com>
2018-08-18 08:56:51 -0700

    Add test for the previous change

--- tests/__tests__/ts-diagnostics.spec.ts
+++ tests/__tests__/ts-diagnostics.spec.ts
@@ -10,4 +10,16 @@ describe('TypeScript Diagnostics errors', () => {
       `Hello.ts(13,10): error TS2339: Property 'unexcuted' does not exist on type 'Hello`,
     );
   });
+
+  it('should only show errors for the file which matches the enableTsDiagnostics regex', () => {
+    const result = runJest('../ts-diagnostics-regex', ['--no-cache']);
+    expect(result.stderr).toContain(
+      `__tests__/Hello-should-diagnose.ts(2,12): error TS2339: Property 'push' does not exist on type`,
+    );
+    expect(result.stderr).toContain(
+      `__tests__/Hello-should-diagnose.ts(13,12): error TS2339: Property 'unexcuted' does not exist on type 'Hello`,
+    );
+
+    expect(result.stderr).not.toContain(`Hello-should-NOT-diagnose.ts`);
+  });
 });
