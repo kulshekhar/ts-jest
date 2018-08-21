6411cee89f51a3557d9c5c685c98be31cef2651d
Andrew Bradley <cspotcode@gmail.com>
2018-08-21 13:38:13 -0400

    Fix use-strict test

--- tests/use-strict/__tests__/Strict.test.ts
+++ tests/use-strict/__tests__/Strict.test.ts
@@ -1,7 +1,7 @@
 import { checkStrict } from '../Strict';
 
 describe('Invalid Strict', () => {
-  it('should throw an error on line 9', () => {
+  it('should throw an error on line 4', () => {
     checkStrict();
   });
 });
