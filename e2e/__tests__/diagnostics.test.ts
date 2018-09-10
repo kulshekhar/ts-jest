f7cc52e799ea59028abe63dc6e9751e2ee241ac9
Huafu Gandon <huafu.gandon@gmail.com>
2018-09-10 16:36:45 +0200

    test(e2e): test case for diagnostics warn only

--- e2e/__tests__/diagnostics.test.ts
+++ e2e/__tests__/diagnostics.test.ts
@@ -1,8 +1,8 @@
 import { allValidPackageSets } from '../__helpers__/templates'
 import { configureTestCase } from '../__helpers__/test-case'
 
-describe('With type checking', () => {
-  const testCase = configureTestCase('type-checking')
+describe('With diagnostics, first throws', () => {
+  const testCase = configureTestCase('diagnostics')
 
   testCase.runWithTemplates(allValidPackageSets, 1, (runTest, { testLabel }) => {
     it(testLabel, () => {
@@ -13,10 +13,8 @@ describe('With type checking', () => {
   })
 })
 
-describe('Without type checking', () => {
-  const testCase = configureTestCase('type-checking', {
-    tsJestConfig: { isolatedModules: true },
-  })
+describe('With diagnostics, warn only', () => {
+  const testCase = configureTestCase('diagnostics', { tsJestConfig: { diagnostics: { warnOnly: true } } })
 
   testCase.runWithTemplates(allValidPackageSets, 0, (runTest, { testLabel }) => {
     it(testLabel, () => {
