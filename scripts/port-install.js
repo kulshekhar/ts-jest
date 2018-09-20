53b6d20a573bc38409a37be20dbcc42117c0fb37
Huafu Gandon <huafu.gandon@gmail.com>
2018-09-20 11:51:50 +0200

    chore(package): adds warning about semver and links in postinstall

---
+++ scripts/port-install.js
@@ -0,0 +1,6 @@
+console.error('WARNING: ts-jest major version number follows jest\'s one. Breaking changes may appears on new minor version number instead of major ones.')
+console.log(`
+    - Documentation: https://kulshekhar.github.io/ts-jest/
+    - Slack community: https://bit.ly/ts-jest-slack
+    - Changelog: https://github.com/kulshekhar/ts-jest/blob/master/CHANGELOG.md.
+`)
