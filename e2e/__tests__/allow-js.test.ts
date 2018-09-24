ad8f6d104a79d450289df0ce41ec63589a869d15
Huafu Gandon <huafu.gandon@gmail.com>
2018-09-24 23:13:58 +0200

    test(preset): e2e test for added presets

--- e2e/__tests__/allow-js.test.ts
+++ e2e/__tests__/allow-js.test.ts
@@ -1,10 +1,30 @@
-import { allValidPackageSets } from '../__helpers__/templates'
+import { allPackageSetsWithPreset, allValidPackageSets } from '../__helpers__/templates'
 import { configureTestCase } from '../__helpers__/test-case'
 
-describe('Allow JS test', () => {
-  const testCase = configureTestCase('allow-js')
+describe('using babel-jest for js files', () => {
+  const testCase = configureTestCase('allow-js', {
+    jestConfig: { testMatch: null, testRegex: '(foo|bar)\\.spec\\.[jt]s$' },
+  })
 
   testCase.runWithTemplates(allValidPackageSets, 0, (runTest, { testLabel }) => {
+    it(testLabel, () => {
+      const result = runTest()
+      expect(result.status).toBe(0)
+      expect(result).toMatchSnapshot()
+    })
+  })
+})
+
+describe('using ts-jest for js files', () => {
+  const testCase = configureTestCase('allow-js', {
+    jestConfig: {
+      preset: 'ts-jest/presets/js-with-ts',
+      testMatch: null,
+      testRegex: 'esm\\.spec\\.[jt]s$',
+    },
+  })
+
+  testCase.runWithTemplates(allPackageSetsWithPreset, 0, (runTest, { testLabel }) => {
     it(testLabel, () => {
       const result = runTest()
       expect(result.status).toBe(0)
