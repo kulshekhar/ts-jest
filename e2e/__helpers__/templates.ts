108b08b90ca428d9acbe91e922f59fb4e5f1f30a
Kulshekhar Kabra <kulshekhar@users.noreply.github.com>
2019-02-16 18:44:14 +0530

    fix: remove unused snapshots

--- e2e/__helpers__/templates.ts
+++ e2e/__helpers__/templates.ts
@@ -1,22 +1,9 @@
 export enum PackageSets {
   default = 'default',
-  babel6 = 'with-babel-6',
   babel7 = 'with-babel-7',
-  jest22 = 'with-jest-22',
   typescript2_7 = 'with-typescript-2-7',
   // invalid
   unsupportedVersion = 'with-unsupported-version',
 }
-export const allValidPackageSets = [
-  PackageSets.default,
-  PackageSets.babel6,
-  PackageSets.babel7,
-  PackageSets.jest22,
-  PackageSets.typescript2_7,
-]
-export const allPackageSetsWithPreset = [
-  PackageSets.default,
-  PackageSets.babel6,
-  PackageSets.babel7,
-  PackageSets.typescript2_7,
-]
+export const allValidPackageSets = [PackageSets.default, PackageSets.babel7, PackageSets.typescript2_7]
+export const allPackageSetsWithPreset = [PackageSets.default, PackageSets.babel7, PackageSets.typescript2_7]
