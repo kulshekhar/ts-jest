dd293c5251071f8905bdfaf181448472bbf3f32f
Micah Chalmer <micah@micahchalmer.net>
2019-02-20 23:39:15 -0500

    fix: correct typings for @types/jest 24
    Bump the version of @types/jest.
    Fix associated typings and tests.
    Also add a test for mocking classes.

--- e2e/__cases__/test-helpers/pass.spec.ts
+++ e2e/__cases__/test-helpers/pass.spec.ts
@@ -1,5 +1,5 @@
 import { mocked } from 'ts-jest/utils'
-import { foo, bar } from './to-mock'
+import { foo, bar, MyClass } from './to-mock'
 jest.mock('./to-mock')
 
 test('foo', () => {
@@ -15,3 +15,17 @@ test('bar', () => {
   expect(mockedBar.dummy.deep.deeper()).toBeUndefined()
   expect(mockedBar.dummy.deep.deeper.mock.calls.length).toBe(1)
 })
+
+test("MyClass", () => {
+  const instance = new MyClass("hi");
+  const otherInstance = new MyClass("there");
+  expect(mocked(MyClass).mock.calls.length).toBe(2);
+
+  const mockedInstance = mocked(instance, true);
+  // mocked always returns undefined by default
+  expect(instance.myStr).toBeUndefined();
+  expect(mockedInstance.myProperty).toBeUndefined();
+  expect(instance.somethingClassy()).toBeUndefined();
+  expect(mockedInstance.somethingClassy.mock.calls.length).toBe(1);
+  expect(mocked(otherInstance).somethingClassy.mock.calls.length).toBe(0);
+});
--- e2e/__cases__/test-helpers/to-mock.ts
+++ e2e/__cases__/test-helpers/to-mock.ts
@@ -13,3 +13,13 @@ export namespace bar {
     }
   }
 }
+
+export class MyClass {
+  constructor(s: string) {
+    this.myProperty = 3
+    this.myStr = s
+  }
+  somethingClassy() { return this.myStr }
+  public myProperty: number;
+  public myStr: string;
+}
