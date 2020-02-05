bb269ac433a08664a05b9a3669cbf1a0b13b3946
Ahn <anhpnnd@gmail.com>
2020-02-05 18:50:47 +0700

    fix(transformers): add deepUnmock to hoist methods

--- e2e/__tests__/hoisting.test.ts
+++ e2e/__tests__/hoisting.test.ts
@@ -2,14 +2,47 @@ import { allValidPackageSets } from '../__helpers__/templates'
 import { configureTestCase } from '../__helpers__/test-case'
 
 describe('Hoisting jest.mock() & jest.unmock()', () => {
-  const testCase = configureTestCase('hoisting', { writeIo: true })
+  const testCase = configureTestCase('hoisting/mock-unmock', {
+    writeIo: true,
+  })
 
   testCase.runWithTemplates(allValidPackageSets, 0, (runTest, { testLabel }) => {
     it(testLabel, () => {
       const result = runTest()
       expect(result.status).toBe(0)
-      expect(result).toMatchSnapshot('output')
-      expect(result.ioFor('hello.spec.ts')).toMatchSnapshot('io')
+      expect(result).toMatchSnapshot('output-mockUnmock')
+      expect(result.ioFor('mock-unmock.spec.ts')).toMatchSnapshot('io-mockUnmock')
+    })
+  })
+})
+
+describe('Hoisting jest.enableAutomock()', () => {
+  const testCase = configureTestCase('hoisting/enable-automock', { writeIo: true })
+
+  testCase.runWithTemplates(allValidPackageSets, 0, (runTest, { testLabel }) => {
+    it(testLabel, () => {
+      const result = runTest()
+      expect(result.status).toBe(0)
+      expect(result).toMatchSnapshot('output-enableAutomock')
+      expect(result.ioFor('enable-automock.spec.ts')).toMatchSnapshot('io-enableAutomock')
+    })
+  })
+})
+
+describe('Hoisting jest.disableAutomock()', () => {
+  const testCase = configureTestCase('hoisting/disable-automock', {
+    writeIo: true,
+    jestConfig: {
+      automock: true,
+    }
+  })
+
+  testCase.runWithTemplates(allValidPackageSets, 0, (runTest, { testLabel }) => {
+    it(testLabel, () => {
+      const result = runTest()
+      expect(result.status).toBe(0)
+      expect(result).toMatchSnapshot('output-disableAutomock')
+      expect(result.ioFor('disable-automock.spec.ts')).toMatchSnapshot('io-disableAutomock')
     })
   })
 })
