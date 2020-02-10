6a1b24caad8e59ee991624d24795aca51623fc48
Ahn <anhpnnd@gmail.com>
2020-02-10 17:27:05 +0700

    fix(compiler): use Program as fallback for language service

--- src/compiler.spec.ts
+++ src/compiler.spec.ts
@@ -2,6 +2,7 @@
 import { Config } from '@jest/types'
 import { LogLevels } from 'bs-logger'
 import { removeSync, writeFileSync } from 'fs-extra'
+import { LanguageService, ModuleKind, ScriptTarget } from 'typescript'
 
 import * as fakers from './__helpers__/fakers'
 import { logTargetMock } from './__helpers__/mocks'
@@ -29,7 +30,41 @@ function makeCompiler({
     pretty: false,
   }
   const cs = new ConfigSet(fakers.jestConfig(jestConfig, tsJestConfig), parentConfig)
+
   return createCompiler(cs)
+}
+
+function makeFallbackCompiler({
+  jestConfig,
+  tsJestConfig,
+  parentConfig,
+}: {
+  jestConfig?: Partial<Config.ProjectConfig>
+  tsJestConfig?: TsJestGlobalOptions
+  parentConfig?: TsJestGlobalOptions
+} = {}) {
+  const compiler = makeCompiler({ jestConfig, tsJestConfig, parentConfig })
+  const languageService: LanguageService = compiler.ts.createLanguageService({
+    getScriptFileNames: jest.fn(),
+    getScriptVersion: jest.fn(),
+    getScriptSnapshot: jest.fn(),
+    getCurrentDirectory: () => '.',
+    getDefaultLibFileName: jest.fn(),
+    getCompilationSettings: () => {
+      return {
+        target: ScriptTarget.ES2018,
+        module: ModuleKind.CommonJS,
+        lib: ['dom', 'es2018'],
+      }
+    },
+  })
+  compiler.ts.createLanguageService = jest.fn().mockReturnValue(languageService)
+  languageService.getEmitOutput = jest.fn().mockReturnValue({
+    outputFiles: [],
+    emitSkipped: false,
+  })
+
+  return compiler
 }
 
 beforeEach(() => {
@@ -83,21 +118,21 @@ describe('cache', () => {
   it('should use the cache', () => {
     const compiled1 = compiler.compile(source, __filename)
     expect(logTarget.filteredLines(LogLevels.debug, Infinity)).toMatchInlineSnapshot(`
-Array [
-  "[level:20] readThrough(): cache miss
-",
-  "[level:20] getOutput(): compiling using language service
-",
-  "[level:20] updateMemoryCache()
-",
-  "[level:20] visitSourceFileNode(): hoisting
-",
-  "[level:20] getOutput(): computing diagnostics
-",
-  "[level:20] readThrough(): writing caches
-",
-]
-`)
+      Array [
+        "[level:20] readThrough(): cache miss
+      ",
+        "[level:20] getOutput(): compiling using language service
+      ",
+        "[level:20] updateMemoryCache()
+      ",
+        "[level:20] visitSourceFileNode(): hoisting
+      ",
+        "[level:20] getOutput(): computing diagnostics from language service
+      ",
+        "[level:20] readThrough(): writing caches
+      ",
+      ]
+    `)
 
     logTarget.clear()
     const compiled2 = compiler.compile(source, __filename)
@@ -159,5 +194,53 @@ console.log(val.p1/* <== that */)
       comment: 'the prop 1!',
       name: '(property) p1: boolean',
     })
+  })
+})
+
+describe('Program', () => {
+  // These preparation steps are needed to make the test work correctly
+  const tmp = tempDir('compiler')
+  let compiler = makeFallbackCompiler({
+    jestConfig: { cache: true, cacheDirectory: tmp },
+    tsJestConfig: { tsConfig: false },
+  })
+  const source = 'console.log("hello")'
+  it('should compile using Program as fallback', () => {
+    try {
+      compiler = makeFallbackCompiler({ tsJestConfig: { tsConfig: false } })
+      compiler.compile(source, 'test.ts')
+    } catch (e) {
+      expect(e.message).toMatchSnapshot('compile-error')
+      expect(logTarget.filteredLines(LogLevels.debug, Infinity)).toMatchInlineSnapshot(`
+        Array [
+          "[level:20] backporting config
+        ",
+          "[level:20] normalized jest config
+        ",
+          "[level:20] normalized ts-jest config
+        ",
+          "[level:20] creating typescript compiler (language service)
+        ",
+          "[level:20] file caching disabled
+        ",
+          "[level:20] normalized typescript config
+        ",
+          "[level:20] creating language service
+        ",
+          "[level:20] readThrough(): no cache
+        ",
+          "[level:20] getOutput(): compiling using language service
+        ",
+          "[level:20] updateMemoryCache()
+        ",
+          "[level:20] getOutput(): creating Program as fallback for language service
+        ",
+          "[level:20] getOutput(): compiling using Program
+        ",
+          "[level:20] getOutput(): computing diagnostics from Program emit result
+        ",
+        ]
+      `)
+    }
   })
 })
