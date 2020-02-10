a34c8531cd69629c3c2f9c3a25c0416d24ad1505
Ahn <anhpnnd@gmail.com>
2020-02-10 16:32:24 +0700

    chore(e2e): add composite e2e test

---
+++ e2e/__templates__/composite/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  projects: [
+    '<rootDir>/packages/*',
+  ],
+}
---
+++ e2e/__templates__/composite/packages/my-library/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'my-library',
+  displayName: 'my-library',
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+}
