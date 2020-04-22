9d54a6196f79215466dcff977884603a412553a8
Ahn <anhpnnd@gmail.com>
2020-04-22 19:01:38 +0200

    chore: release 25.5.0-alpha.0

--- src/compiler/transpiler.ts
+++ src/compiler/transpiler.ts
@@ -1,5 +1,4 @@
 import { Logger } from 'bs-logger'
-import { normalize } from 'path'
 import * as _ts from 'typescript'
 
 import { ConfigSet } from '../config/config-set'
@@ -26,6 +25,7 @@ export const initializeTranspilerInstance = (
         projectReferences,
       })
     : ts.createProgram([], options)
+  /* istanbul ignore next (we leave this for e2e) */
   const updateFileInCache = (contents: string, filePath: string) => {
     const file = memoryCache.files.get(filePath)
     if (file && file.text !== contents) {
@@ -36,31 +36,24 @@ export const initializeTranspilerInstance = (
 
   return {
     compileFn: (code: string, fileName: string): SourceOutput => {
-      const normalizedFileName = normalize(fileName)
-
-      updateFileInCache(code, normalizedFileName)
-      const referencedProject = getAndCacheProjectReference(
-        normalizedFileName,
-        program,
-        memoryCache.files,
-        projectReferences,
-      )
+      updateFileInCache(code, fileName)
+      const referencedProject = getAndCacheProjectReference(fileName, program, memoryCache.files, projectReferences)
       /* istanbul ignore next (referencedProject object is too complex to mock so we leave this for e2e) */
       if (referencedProject !== undefined) {
-        logger.debug({ normalizedFileName }, 'compileFn(): get compile result from referenced project')
+        logger.debug({ fileName }, 'compileFn(): get compile result from referenced project')
 
-        return getCompileResultFromReferencedProject(normalizedFileName, configs, memoryCache.files, referencedProject)
+        return getCompileResultFromReferencedProject(fileName, configs, memoryCache.files, referencedProject)
       } else {
-        logger.debug({ normalizedFileName }, 'compileFn(): compiling as isolated module')
+        logger.debug({ fileName }, 'compileFn(): compiling as isolated module')
 
         const result: _ts.TranspileOutput = ts.transpileModule(code, {
-          fileName: normalizedFileName,
+          fileName,
           transformers: configs.tsCustomTransformers,
           compilerOptions: options,
-          reportDiagnostics: configs.shouldReportDiagnostic(normalizedFileName),
+          reportDiagnostics: configs.shouldReportDiagnostic(fileName),
         })
-        if (result.diagnostics && configs.shouldReportDiagnostic(normalizedFileName)) {
-          configs.raiseDiagnostics(result.diagnostics, normalizedFileName, logger)
+        if (result.diagnostics && configs.shouldReportDiagnostic(fileName)) {
+          configs.raiseDiagnostics(result.diagnostics, fileName, logger)
         }
 
         return [result.outputText, result.sourceMapText!]
