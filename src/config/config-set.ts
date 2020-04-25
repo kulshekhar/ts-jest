23d818c95a8e920d40b238121304c07958d9c8b2
Ahn <anhpnnd@gmail.com>
2020-04-25 08:15:48 +0200

    fix(compiler)(**BREAKING**): allow using `files` provided by `tsconfig` (#1562)
    BREAKING CHANGE:
    
    Any custom typing files or files which are needed to be compiled and intended to use with `jest` need to be defined in `files` option of `tsconfig`.
    
    For example
    ```
    // tsconfig.json
    {
       // ...other configs
       "files": [
           "my-custom-typings.d.ts".
           "my-global-module.ts"
       ]
    }
    ```

--- src/config/config-set.ts
+++ src/config/config-set.ts
@@ -717,7 +717,7 @@ export class ConfigSet {
     resolvedConfigFile?: string | null,
     noProject?: boolean | null,
   ): ReadTsConfigResult {
-    let config = { compilerOptions: {} }
+    let config = { compilerOptions: {}, include: [] }
     let basePath = normalizeSlashes(this.rootDir)
     let configFileName: string | undefined
     const ts = this.compilerModule
@@ -759,6 +759,11 @@ export class ConfigSet {
       ...config.compilerOptions,
       ...compilerOptions,
     }
+    /**
+     * Always set include to empty array so fileNames after parseJsonConfigFileContent only contains the least minimum initial
+     * files to utilize LanguageService incremental feature
+     */
+    config.include = []
 
     // parse json, merge config extending others, ...
     const result = ts.parseJsonConfigFileContent(config, ts.sys, basePath, undefined, configFileName)
