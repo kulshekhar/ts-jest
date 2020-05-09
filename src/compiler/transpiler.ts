e167564204f2c03c1f739139bba818fa680dec70
Ahn <anhpnnd@gmail.com>
2020-05-09 08:07:17 +0200

    fix(compiler): don't resolve files from build folder for `projectReferences` (#1614)

--- src/compiler/transpiler.ts
+++ src/compiler/transpiler.ts
@@ -4,8 +4,6 @@ import * as _ts from 'typescript'
 import { ConfigSet } from '../config/config-set'
 import { CompilerInstance, MemoryCache, SourceOutput } from '../types'
 
-import { getAndCacheProjectReference, getCompileResultFromReferencedProject } from './compiler-utils'
-
 /**
  * @internal
  */
@@ -16,15 +14,9 @@ export const initializeTranspilerInstance = (
 ): CompilerInstance => {
   logger.debug('initializeTranspilerInstance(): create typescript compiler')
 
-  const { options, projectReferences, fileNames } = configs.parsedTsConfig
+  const { options, fileNames } = configs.parsedTsConfig
   const ts = configs.compilerModule
-  const program = projectReferences
-    ? ts.createProgram({
-        rootNames: fileNames,
-        options,
-        projectReferences,
-      })
-    : ts.createProgram([], options)
+  const program = ts.createProgram(fileNames, options)
   /* istanbul ignore next (we leave this for e2e) */
   const updateFileInCache = (contents: string, filePath: string) => {
     const file = memoryCache.files.get(filePath)
@@ -37,27 +29,20 @@ export const initializeTranspilerInstance = (
   return {
     compileFn: (code: string, fileName: string): SourceOutput => {
       updateFileInCache(code, fileName)
-      const referencedProject = getAndCacheProjectReference(fileName, program, memoryCache.files, projectReferences)
-      /* istanbul ignore next (referencedProject object is too complex to mock so we leave this for e2e) */
-      if (referencedProject !== undefined) {
-        logger.debug({ fileName }, 'compileFn(): get compile result from referenced project')
 
-        return getCompileResultFromReferencedProject(fileName, configs, memoryCache.files, referencedProject)
-      } else {
-        logger.debug({ fileName }, 'compileFn(): compiling as isolated module')
+      logger.debug({ fileName }, 'compileFn(): compiling as isolated module')
 
-        const result: _ts.TranspileOutput = ts.transpileModule(code, {
-          fileName,
-          transformers: configs.tsCustomTransformers,
-          compilerOptions: options,
-          reportDiagnostics: configs.shouldReportDiagnostic(fileName),
-        })
-        if (result.diagnostics && configs.shouldReportDiagnostic(fileName)) {
-          configs.raiseDiagnostics(result.diagnostics, fileName, logger)
-        }
-
-        return [result.outputText, result.sourceMapText!]
+      const result: _ts.TranspileOutput = ts.transpileModule(code, {
+        fileName,
+        transformers: configs.tsCustomTransformers,
+        compilerOptions: options,
+        reportDiagnostics: configs.shouldReportDiagnostic(fileName),
+      })
+      if (result.diagnostics && configs.shouldReportDiagnostic(fileName)) {
+        configs.raiseDiagnostics(result.diagnostics, fileName, logger)
       }
+
+      return [result.outputText, result.sourceMapText!]
     },
     program,
   }
