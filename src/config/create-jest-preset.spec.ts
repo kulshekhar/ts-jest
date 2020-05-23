52641192a7ac22b7d0681ae20402fdf61d203eee
Ahn <anhpnnd@gmail.com>
2020-05-23 20:26:27 +0200

    chore(dev-infra): create TsJestPresets typings based on Jest typings

--- src/config/create-jest-preset.spec.ts
+++ src/config/create-jest-preset.spec.ts
@@ -1,33 +1,35 @@
 import { createJestPreset } from './create-jest-preset'
 
-it('should return correct defaults when allowJs is false or not set', () => {
-  const withoutJs = {
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-    },
-  }
-  expect(createJestPreset()).toEqual(withoutJs)
-  expect(createJestPreset({ allowJs: false })).toEqual(withoutJs)
-})
-
-it('should return correct defaults when allowJs is true', () => {
-  expect(createJestPreset({ allowJs: true })).toEqual({
-    transform: {
-      '^.+\\.[tj]sx?$': 'ts-jest',
-    },
+describe('create-jest-preset', () => {
+  it('should return correct defaults when allowJs is false or not set', () => {
+    const withoutJs = {
+      transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+      },
+    }
+    expect(createJestPreset()).toEqual(withoutJs)
+    expect(createJestPreset({ allowJs: false })).toEqual(withoutJs)
   })
-})
 
-it('should be able to use a base config', () => {
-  expect(createJestPreset(undefined, {})).toMatchInlineSnapshot(`
+  it('should return correct defaults when allowJs is true', () => {
+    expect(createJestPreset({ allowJs: true })).toEqual({
+      transform: {
+        '^.+\\.[tj]sx?$': 'ts-jest',
+      },
+    })
+  })
+
+  it('should be able to use a base config', () => {
+    expect(createJestPreset(undefined, {})).toMatchInlineSnapshot(`
 Object {
   "transform": Object {
     "^.+\\\\.tsx?$": "ts-jest",
   },
 }
 `)
-  expect(createJestPreset(undefined, { testMatch: ['foo'], moduleFileExtensions: ['bar'], transform: { foo: 'bar' } }))
-    .toMatchInlineSnapshot(`
+    expect(
+      createJestPreset(undefined, { testMatch: ['foo'], moduleFileExtensions: ['bar'], transform: { foo: 'bar' } }),
+    ).toMatchInlineSnapshot(`
 Object {
   "moduleFileExtensions": Array [
     "bar",
@@ -41,4 +43,5 @@ Object {
   },
 }
 `)
+  })
 })
