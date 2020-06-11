561677379b0cd091384de934358fb53b9df64485
Ahn <anhpnnd@gmail.com>
2020-06-11 09:48:30 +0200

    fix(config): show version warning when using ts-jest without babel

--- e2e/__tests__/jest-presets.test.ts
+++ e2e/__tests__/jest-presets.test.ts
@@ -1,4 +1,4 @@
-import { allPackageSetsWithPreset } from '../__helpers__/templates'
+import { PackageSets } from '../__helpers__/templates'
 import { configureTestCase } from '../__helpers__/test-case'
 
 // 'ts-jest' is tested in almost all test cases
@@ -8,11 +8,19 @@ import { configureTestCase } from '../__helpers__/test-case'
 describe('ts-jest/presets/js-with-babel', () => {
   const testCase = configureTestCase('preset-with-babel', { jestConfig: { preset: 'ts-jest/presets/js-with-babel' } })
 
-  testCase.runWithTemplates(allPackageSetsWithPreset, 1, (runTest, { testLabel }) => {
+  testCase.runWithTemplates([PackageSets.default], 1, (runTest, { testLabel }) => {
     it(testLabel, () => {
       const result = runTest()
       expect(result.status).toBe(1)
       expect(result.stderr).toMatch(/(Couldn't|Cannot) find (preset|module) ["']@babel\/preset-env["']/)
+    })
+  })
+
+  testCase.runWithTemplates([PackageSets.babel7, PackageSets.babel7StringConfig], 0, (runTest, { testLabel }) => {
+    it(testLabel, () => {
+      const result = runTest()
+      expect(result.status).toBe(0)
+      expect(result).toMatchSnapshot()
     })
   })
 })
