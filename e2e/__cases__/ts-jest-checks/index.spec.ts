69488552eca2846f3fc6ba86ab49d7893caaf521
Ahn <anhpnnd@gmail.com>
2020-07-09 16:20:39 +0200

    fix(config): invalidate cache when other options in `tsconfig` change (#1788)

--- e2e/__cases__/ts-jest-checks/index.spec.ts
+++ e2e/__cases__/ts-jest-checks/index.spec.ts
@@ -4,7 +4,7 @@ import { readFileSync } from 'fs'
 describe('tsConfig', () => {
   it('should have digest and versions', () => {
     const tr = createTransformer()
-    const cs = tr.configsFor({} as any)
+    const cs = tr.configsFor(Object.create(null))
     expect(cs.tsJestDigest).toHaveLength(40)
     expect(cs.tsJestDigest).toBe(readFileSync(require.resolve('ts-jest/.ts-jest-digest'), 'utf8'))
     expect(cs.versions['ts-jest']).toBe(require('ts-jest/package.json').version)
