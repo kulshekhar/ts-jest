0caf85fcbc9906ac64efc2761532d9bbd66392c7
Sebastian Nemeth <sebastian@monkmage.com>
2020-07-15 15:57:21 +0300

    fix(mapper): snapshot paths not matching in windows environment

--- src/config/projects-to-module-name-mapper.spec.ts
+++ src/config/projects-to-module-name-mapper.spec.ts
@@ -1,3 +1,4 @@
+import * as path from 'path'
 import { projectsToModuleNameMapper } from './projects-to-module-name-mapper'
 
 const tsConfigReferences = [
@@ -21,10 +22,10 @@ describe('projectsToModuleNameMapper', () => {
 
     expect(mapper).toMatchInlineSnapshot(`
       Object {
-        "@ts-jest/project-1": "${__dirname}/__helpers__/project-1",
-        "@ts-jest/project-2": "${__dirname}/__helpers__/project-2/src/index",
-        "@ts-jest/project-3": "${__dirname}/__helpers__/project-3/index",
-        "@ts-jest/project-4": "${__dirname}/__helpers__/project-4/someDir/index",
+        "@ts-jest/project-1": "${path.join(__dirname, `/__helpers__/project-1`)}",
+        "@ts-jest/project-2": "${path.join(__dirname, `/__helpers__/project-2/src/index`)}",
+        "@ts-jest/project-3": "${path.join(__dirname, `/__helpers__/project-3/index`)}",
+        "@ts-jest/project-4": "${path.join(__dirname, `/__helpers__/project-4/someDir/index`)}",
       }
     `)
   })
--- src/config/tsconfig-to-module-name-mapper.spec.ts
+++ src/config/tsconfig-to-module-name-mapper.spec.ts
@@ -8,10 +8,10 @@ describe('tsConfigToModuleNameMapper', () => {
 
     expect(mapper).toMatchInlineSnapshot(`
       Object {
-        "@ts-jest/project-1": "${__dirname}/__helpers__/project-1",
-        "@ts-jest/project-2": "${__dirname}/__helpers__/project-2/src/index",
-        "@ts-jest/project-3": "${__dirname}/__helpers__/project-3/index",
-        "@ts-jest/project-4": "${__dirname}/__helpers__/project-4/someDir/index",
+        "@ts-jest/project-1": "${path.join(__dirname, `/__helpers__/project-1`)}",
+        "@ts-jest/project-2": "${path.join(__dirname, `/__helpers__/project-2/src/index`)}",
+        "@ts-jest/project-3": "${path.join(__dirname, `/__helpers__/project-3/index`)}",
+        "@ts-jest/project-4": "${path.join(__dirname, `/__helpers__/project-4/someDir/index`)}",
         "^api/(.*)$": "src/api/$1",
         "^client$": Array [
           "src/client",
