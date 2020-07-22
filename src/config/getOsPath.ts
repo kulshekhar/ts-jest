f0438ee8699820d2956546faca9738aa173c73db
Sebastian Nemeth <sebastian@monkmage.com>
2020-07-22 14:31:20 +0300

    fix(mapper): specs fail in win32 due to double slash notation

--- src/config/getOsPath.ts
+++ src/config/getOsPath.ts
@@ -3,5 +3,5 @@ import * as path from 'path'
 export const getOsPath = (...paths: string[]): string => {
   const joinedPath = path.join(...paths)
 
-  return process.platform === 'win32' ? joinedPath.replace('\\', '\\') : joinedPath
+  return process.platform === 'win32' ? joinedPath.replace('\\', '\\\\') : joinedPath
 }
