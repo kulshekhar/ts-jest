1fa3ea43d065601de87d4b77d81cb1748b73c71f
Ahn <anhpnnd@gmail.com>
2020-10-24 21:42:29 +0200

    fix(compiler): only exclude test files when initializing compiler
    Closes #2061

--- e2e/__external-repos__/custom-typings/__tests__/index.test.ts
+++ e2e/__external-repos__/custom-typings/__tests__/index.test.ts
@@ -1,9 +1,13 @@
-import $ from "jquery"
-
 function foo() {
     $.ajax<number>("/foo")
 }
 
-test("foo", () => {
+test('foo', () => {
   expect(2).toBe(2)
+})
+
+test('bar', () => {
+  const terminateReason: TerminatedReason = 'culled'
+
+  expect(terminateReason).toEqual('culled')
 })
