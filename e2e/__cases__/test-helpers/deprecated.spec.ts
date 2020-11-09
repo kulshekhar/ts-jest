99468a38bd2a4521ff24af740f1b33040399cc11
Ahn <anhpnnd@gmail.com>
2020-11-09 15:13:10 +0100

    refactor: move jest transformer class to package entry
    BREAKING CHANGE:
    - One currently refers type in `jest.config.js`
    ```
    /** @typedef {import('ts-jest')} */
    module.exports = {
      //...
    }
    ```
    
    should change to
    ```
    /** @typedef {import('ts-jest/dist/types')} */
    module.exports = {
      //...
    }
    ```
    
    - Remove possibilities to import `mocked`, `createJestPreset`, `pathsToModuleNameMapper` from package entry. One should change to
    ```
    import { mocked, createJestPreset, pathsToModuleNameMapper` } from 'ts-jest/utils'
    ```

--- e2e/__cases__/test-helpers/deprecated.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mocked } from 'ts-jest'
-import { foo } from './pass-to-mock'
-jest.mock('./pass-to-mock')
-
-test('foo', () => {
-  foo()
-  // it should log that the helper moved
-  expect(mocked(foo).mock.calls.length).toBe(1)
-})
