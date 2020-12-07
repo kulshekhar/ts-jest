ca8d7c44779105ef208d17cdd8c6baaf96b479ad
Ahn <anhpnnd@gmail.com>
2020-12-07 14:06:34 +0100

    feat: allow subclasses of `TsJestTransformer` to have version checking (#2176)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-import { VersionCheckers } from './utils/version-checkers'
 import { TsJestTransformer } from './ts-jest-transformer'
 
 export function createTransformer(): TsJestTransformer {
-  VersionCheckers.jest.warn()
-
   return new TsJestTransformer()
 }
