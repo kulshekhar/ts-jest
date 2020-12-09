cf85e26f119c5c056541c878b2a4051bbd416866
Ahn <anhpnnd@gmail.com>
2020-12-09 12:19:34 +0100

    Deploy website - based on 08e5a18667b6e6c6d4b64e61e9059b4f14b0df27

--- assets/js/anchors.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    'use strict';
-    $(':header[id]').each(function() {
-      var $this = $(this);
-      if(!/^h[2-6]$/i.test($this.prop('nodeName'))) return;
-      $this.prepend('<a class="auto-anchor text-light" href="#' + $this.attr('id') + '"><i class="fas fa-link"></i></a>')
-    });
-  });
