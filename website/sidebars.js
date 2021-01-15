b2935860ad22603f09f3bd35a43d02d631e60671
Ahn <anhpnnd@gmail.com>
2021-01-15 10:37:10 +0100

    docs(devs-infra): reorganize sidebar and add troubleshooting section

--- website/sidebars.js
+++ website/sidebars.js
@@ -7,27 +7,24 @@ module.exports = {
     {
       type: 'category',
       label: 'Configuration',
-      items: ['presets', 'paths-mapping', 'options', 'version-checking', 'migration'],
+      items: ['presets', 'paths-mapping', 'options', 'version-checking'],
     },
     {
-      type: 'doc',
-      id: 'test-helpers',
-    },
-    {
-      type: 'doc',
-      id: 'esm-support',
-    },
-    {
-      type: 'doc',
-      id: 'react-native',
+      type: 'category',
+      label: 'Guides',
+      items: ['test-helpers', 'esm-support', 'react-native', 'troubleshooting'],
     },
     {
       type: 'doc',
       id: 'babel7-or-ts',
     },
     {
+      type: 'doc',
+      id: 'migration',
+    },
+    {
       type: 'category',
-      label: 'Contribution',
+      label: 'How it works',
       items: ['processing', 'e2e-testing'],
     },
   ],
