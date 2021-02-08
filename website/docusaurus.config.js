3d3e0b4e67361f22e589637b35effa3470d74a1e
Ahn <anhpnnd@gmail.com>
2021-02-08 12:27:03 +0100

    docs(devs-infra): correct edit link location (#2333)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -84,8 +84,7 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/kulshekhar/ts-jest/edit/master/docs',
+          editUrl: 'https://github.com/kulshekhar/ts-jest/edit/master/website',
           remarkPlugins: [simplePlantUML],
         },
         theme: {
