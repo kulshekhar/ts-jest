09243cfd348fb2f222346fb50edfb475a2472f55
ahnpnl <27772165+ahnpnl@users.noreply.github.com>
2021-11-29 15:29:54 +0100

    refactor(utils): deprecate `mocked` util function

--- src/utils/testing.ts
+++ src/utils/testing.ts
@@ -1,3 +1,12 @@
+if (!process.env.DISABLE_MOCKED_WARNING) {
+  console.warn(
+    '\n`mocked` util function is now deprecated and has been moved to Jest repository,' +
+      ' see https://github.com/facebook/jest/pull/12089. In `ts-jest` v28.0.0, `mocked` function will be completely removed.' +
+      ' Users are encouraged to use to Jest v27.4.0 or above to have `mocked` function available from `jest-mock`. ' +
+      'One can disable this warning by setting environment variable process.env.DISABLE_MOCKED_WARNING=true\n',
+  )
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type MockableFunction = (...args: any[]) => any
 export type MethodKeysOf<T> = { [K in keyof T]: T[K] extends MockableFunction ? K : never }[keyof T]
