2a526f8594299a7cdacffd36ea01d27fefe2e456
anh.pham <anh.pham@cm.com>
2022-04-26 21:04:02 +0200

    feat(presets): add presets for legacy mode

--- src/legacy/index.ts
+++ src/legacy/index.ts
@@ -1,7 +1,5 @@
 import { TsJestTransformer } from './ts-jest-transformer'
 
-export * from './ts-jest-transformer'
-
 export default {
   createTransformer: (): TsJestTransformer => new TsJestTransformer(),
 }
