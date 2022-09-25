eabe906e1dd6b132a7b0d05ffc13172cd8a6b73b
jjangga0214 <bnbcmindnpass@gmail.com>
2022-09-26 00:02:18 +0900

    feat: add `useESM` option to `pathsToModuleNameMapper` options (#3792)

--- src/config/paths-to-module-name-mapper.spec.ts
+++ src/config/paths-to-module-name-mapper.spec.ts
@@ -39,6 +39,33 @@ describe('pathsToModuleNameMapper', () => {
     `)
   })
 
+  test('should add `js` extension to resolved config with useESM: true', () => {
+    expect(pathsToModuleNameMapper(tsconfigMap, { useESM: true })).toEqual({
+      /**
+       * Why not using snapshot here?
+       * Because the snapshot does not keep the property order, which is important for jest.
+       * A pattern ending with `\\.js` should appear before another pattern without the extension does.
+       */
+      '^log$': 'src/utils/log',
+      '^server$': 'src/server',
+      '^client$': ['src/client', 'src/client/index'],
+      '^util/(.*)\\.js$': 'src/utils/$1',
+      '^util/(.*)$': 'src/utils/$1',
+      '^api/(.*)\\.js$': 'src/api/$1',
+      '^api/(.*)$': 'src/api/$1',
+      '^test/(.*)\\.js$': 'test/$1',
+      '^test/(.*)$': 'test/$1',
+      '^mocks/(.*)\\.js$': 'test/mocks/$1',
+      '^mocks/(.*)$': 'test/mocks/$1',
+      '^test/(.*)/mock\\.js$': ['test/mocks/$1', 'test/__mocks__/$1'],
+      '^test/(.*)/mock$': ['test/mocks/$1', 'test/__mocks__/$1'],
+      '^@foo\\-bar/common$': '../common/dist/library',
+      '^@pkg/(.*)\\.js$': './packages/$1',
+      '^@pkg/(.*)$': './packages/$1',
+      '^(\\.{1,2}/.*)\\.js$': '$1',
+    })
+  })
+
   test.each(['<rootDir>/', 'foo'])('should convert tsconfig mapping with given prefix', (prefix) => {
     expect(pathsToModuleNameMapper(tsconfigMap, { prefix })).toMatchSnapshot(prefix)
   })
