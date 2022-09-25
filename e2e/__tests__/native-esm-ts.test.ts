5a54a63e1d200d4a2c9f65f1b7477408471f910d
Ahn <27772165+ahnpnl@users.noreply.github.com>
2022-09-25 18:42:15 +0200

    test(e2e): change esm nodejs condition to 14.15.0 (#3835)

--- e2e/__tests__/native-esm-ts.test.ts
+++ e2e/__tests__/native-esm-ts.test.ts
@@ -1,7 +1,7 @@
 import { onNodeVersions, json as runWithJson } from '../run-jest'
 
 // The versions where vm.Module exists and commonjs with "exports" is not broken
-onNodeVersions('>=12.16.0', () => {
+onNodeVersions('>=14.15.0', () => {
   test('runs TS test with native ESM', () => {
     const { exitCode, json } = runWithJson('native-esm-ts', [], {
       nodeOptions: '--experimental-vm-modules --no-warnings',
