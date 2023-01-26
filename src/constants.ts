b8f6eaa66662f6463c330645129c07a86722a2b2
smack0007 <zachary.snow@gmail.com>
2023-01-26 12:42:32 +0100

    fix(transformer): allow transforming of .cts/.mts extensions. (#3996)

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 export const LINE_FEED = '\n'
 export const DECLARATION_TYPE_EXT = '.d.ts'
 export const JS_JSX_EXTENSIONS = ['.js', '.jsx']
-export const TS_TSX_REGEX = /\.m?tsx?$/
-export const JS_JSX_REGEX = /\.m?jsx?$/
+export const TS_TSX_REGEX = /\.[cm]?tsx?$/
+export const JS_JSX_REGEX = /\.[cm]?jsx?$/
 // `extensionsToTreatAsEsm` will throw error with `.mjs`
 export const TS_EXT_TO_TREAT_AS_ESM = ['.ts', '.tsx', '.mts']
 export const JS_EXT_TO_TREAT_AS_ESM = ['.jsx']
