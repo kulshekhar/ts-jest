6c6b3786177719af47dfc38be00322f9c150a71e
Marco Pasqualetti <marco.pasqualetti@live.com>
2023-03-26 20:30:25 +0200

    test(custom-compiler): skip due to ttypescript lacks ts@5 support so far

--- e2e/__tests__/custom-compiler.test.ts
+++ e2e/__tests__/custom-compiler.test.ts
@@ -5,7 +5,19 @@ import { runNpmInstall } from '../utils'
 
 const DIR_NAME = 'custom-compiler'
 
-describe('ttypescript', () => {
+/**
+ * Test disabled after typescript@5 support due to issues on ttypescript.
+ * Now typescript exports module rather than object namespaces so ttypescript is not working anymore.
+ * @see https://github.com/kulshekhar/ts-jest/pull/4064#issuecomment-1483937671
+ *
+ * Additional info can be found on the following links:
+ * @see https://github.com/cevek/ttypescript/issues/140
+ * @see https://github.com/microsoft/TypeScript/issues/52826
+ *
+ * Test can be re-enabled after the library supports typescript@5
+ */
+// eslint-disable-next-line jest/no-disabled-tests
+describe.skip('ttypescript', () => {
   const TTS_DIR_NAME = 'ttypescript'
 
   beforeAll(() => {
