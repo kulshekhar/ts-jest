809fcffe4b480859c43512538dcb31bc2461ea1c
Anh Pham <anh.pham@cm.com>
2024-01-19 11:17:46 +0100

    test(e2e): remove `onNodeVersions` function
    Since we support only Node 16+, we can safely remove this function

--- e2e/__tests__/extend-ts-jest.test.ts
+++ e2e/__tests__/extend-ts-jest.test.ts
@@ -1,15 +1,12 @@
-import { json as runWithJson, onNodeVersions } from '../run-jest'
+import { json as runWithJson } from '../run-jest'
 
 const DIR = 'extend-ts-jest'
 
-// Only need to test in ESM because ESM makes `this` context become `undefined`
-onNodeVersions('>=14.15.0', () => {
-  test(`successfully runs the tests inside ${DIR}`, () => {
-    const { json } = runWithJson(DIR, undefined, {
-      nodeOptions: '--experimental-vm-modules --no-warnings',
-    })
-
-    expect(json.success).toBe(true)
-    expect(json.numTotalTestSuites).toBe(1)
+test(`successfully runs the tests inside ${DIR}`, () => {
+  const { json } = runWithJson(DIR, undefined, {
+    nodeOptions: '--experimental-vm-modules --no-warnings',
   })
+
+  expect(json.success).toBe(true)
+  expect(json.numTotalTestSuites).toBe(1)
 })
