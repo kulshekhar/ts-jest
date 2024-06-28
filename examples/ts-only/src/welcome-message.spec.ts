f33bcf7b8c6ab702d4f5cab2daadfe76fa2d4ea6
ahnpnl <anhpnnd@gmail.com>
2024-06-28 11:19:00 +0200

    feat: add test for resource management

--- examples/ts-only/src/welcome-message.spec.ts
+++ examples/ts-only/src/welcome-message.spec.ts
@@ -1,5 +1,14 @@
-import { getWelcomeMessage } from './welcome-message'
+import { join } from 'path'
+import { cwd } from 'process'
+
+import { getWelcomeMessage, processFile } from './welcome-message'
 
 test('should show welcome message', () => {
   expect(getWelcomeMessage()).toMatchInlineSnapshot(`"Welcome to ts-jest!!!"`)
+})
+
+test('should work with resource management', async () => {
+  const result = await processFile(join(cwd(), 'src', 'data.txt'))
+
+  expect(result).toContain('hello')
 })
--- examples/ts-only/src/welcome-message.ts
+++ examples/ts-only/src/welcome-message.ts
@@ -1,1 +1,35 @@
 export const getWelcomeMessage = (): string => 'Welcome to ts-jest!!!'
+
+import { FileHandle, open } from 'fs/promises'
+
+// @ts-expect-error testing
+Symbol['asyncDispose'] ??= Symbol("Symbol.asyncDispose");
+
+class DisposableFileHandle {
+  private fileHandle: FileHandle
+
+  private constructor(fileHandle: FileHandle) {
+    this.fileHandle = fileHandle
+  }
+
+  static async open(filePath: string): Promise<DisposableFileHandle> {
+    const fileHandle = await open(filePath, 'r')
+
+    return new DisposableFileHandle(fileHandle)
+  }
+
+  async readFile(encoding: BufferEncoding): Promise<string> {
+    return this.fileHandle.readFile({ encoding })
+  }
+
+  [Symbol.asyncDispose] = async () => {
+    await this.fileHandle.close()
+  }
+}
+
+export async function processFile(filePath: string) {
+  // eslint-disable-next-line prettier/prettier
+  await using fileHandle = await DisposableFileHandle.open(filePath)
+
+  return await fileHandle.readFile('utf-8')
+}
