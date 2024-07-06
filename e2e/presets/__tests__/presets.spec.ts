1e15ec896f1de5465716dc1e82bc5ec5f5c234b5
ahnpnl <anhpnnd@gmail.com>
2024-07-06 02:41:15 +0200

    refactor(presets): use create preset util functions for cjs presets

--- e2e/presets/__tests__/presets.spec.ts
+++ e2e/presets/__tests__/presets.spec.ts
@@ -1,74 +1,54 @@
-import { JS_EXT_TO_TREAT_AS_ESM, TS_EXT_TO_TREAT_AS_ESM } from 'ts-jest'
+import {
+  JS_EXT_TO_TREAT_AS_ESM,
+  TS_EXT_TO_TREAT_AS_ESM,
+  createDefaultPreset,
+  createDefaultEsmPreset,
+  createJsWithTsPreset,
+  createJsWithTsEsmPreset,
+  createJsWithBabelPreset,
+  createJsWithBabelEsmPreset,
+} from 'ts-jest'
+// @ts-expect-error we have this file but we don't have type definition for it
+import defaultPreset from 'ts-jest/jest-preset'
 import presets from 'ts-jest/presets'
 
 import extendedJestConfig from '../extended-jest-config'
 
-const defaultPreset = require('ts-jest/jest-preset')
-
 test('presets', () => {
-  expect(defaultPreset).toEqual({
-    transform: {
-      '^.+\\.tsx?$': ['ts-jest', {}],
-    },
-  })
+  expect(defaultPreset).toEqual(createDefaultPreset())
   expect(presets.defaultsLegacy).toEqual({
     transform: {
       '^.+\\.tsx?$': ['ts-jest/legacy', {}],
     },
   })
-  expect(presets.defaultsESM).toEqual({
-    extensionsToTreatAsEsm: [...TS_EXT_TO_TREAT_AS_ESM],
-    transform: {
-      '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
-    },
-  })
+  expect(presets.defaultsESM).toEqual(createDefaultEsmPreset())
   expect(presets.defaultsESMLegacy).toEqual({
     extensionsToTreatAsEsm: [...TS_EXT_TO_TREAT_AS_ESM],
     transform: {
       '^.+\\.tsx?$': ['ts-jest/legacy', { useESM: true }],
     },
   })
-  expect(presets.jsWithTs).toEqual({
-    transform: {
-      '^.+\\.[tj]sx?$': ['ts-jest', {}],
-    },
-  })
+  expect(presets.jsWithTs).toEqual(createJsWithTsPreset())
   expect(presets.jsWithTsLegacy).toEqual({
     transform: {
       '^.+\\.[tj]sx?$': ['ts-jest/legacy', {}],
     },
   })
-  expect(presets.jsWithTsESM).toEqual({
-    extensionsToTreatAsEsm: [...JS_EXT_TO_TREAT_AS_ESM, ...TS_EXT_TO_TREAT_AS_ESM],
-    transform: {
-      '^.+\\.m?[tj]sx?$': ['ts-jest', { useESM: true }],
-    },
-  })
+  expect(presets.jsWithTsESM).toEqual(createJsWithTsEsmPreset())
   expect(presets.jsWithTsESMLegacy).toEqual({
     extensionsToTreatAsEsm: [...JS_EXT_TO_TREAT_AS_ESM, ...TS_EXT_TO_TREAT_AS_ESM],
     transform: {
       '^.+\\.m?[tj]sx?$': ['ts-jest/legacy', { useESM: true }],
     },
   })
-  expect(presets.jsWithBabel).toEqual({
-    transform: {
-      '^.+\\.tsx?$': ['ts-jest', {}],
-      '^.+\\.jsx?$': 'babel-jest',
-    },
-  })
+  expect(presets.jsWithBabel).toEqual(createJsWithBabelPreset())
   expect(presets.jsWithBabelLegacy).toEqual({
     transform: {
       '^.+\\.tsx?$': ['ts-jest/legacy', {}],
       '^.+\\.jsx?$': 'babel-jest',
     },
   })
-  expect(presets.jsWithBabelESM).toEqual({
-    extensionsToTreatAsEsm: [...JS_EXT_TO_TREAT_AS_ESM, ...TS_EXT_TO_TREAT_AS_ESM],
-    transform: {
-      '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
-      '^.+\\.m?[j]sx?$': 'babel-jest',
-    },
-  })
+  expect(presets.jsWithBabelESM).toEqual(createJsWithBabelEsmPreset())
   expect(presets.jsWithBabelESMLegacy).toEqual({
     extensionsToTreatAsEsm: [...JS_EXT_TO_TREAT_AS_ESM, ...TS_EXT_TO_TREAT_AS_ESM],
     transform: {
