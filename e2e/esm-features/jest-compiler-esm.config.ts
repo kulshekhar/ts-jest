0874cf5404554e48525ab537465dfce4dd79347f
ahnpnl <anhpnnd@gmail.com>
2024-08-19 15:22:58 +0200

    test: add e2e test case for ESM features

---
+++ e2e/esm-features/jest-compiler-esm.config.ts
@@ -0,0 +1,15 @@
+import { type JestConfigWithTsJest, TS_TRANSFORM_PATTERN } from 'ts-jest'
+
+export default {
+  displayName: 'esm-features-compiler-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    [TS_TRANSFORM_PATTERN]: [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig-esm.spec.json',
+        useESM: true,
+      },
+    ],
+  },
+} satisfies JestConfigWithTsJest
