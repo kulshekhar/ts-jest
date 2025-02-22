69b863d2a6930fb8e487ad5a66308609c8c37892
ahnpnl <anhpnnd@gmail.com>
2025-02-22 12:21:44 +0100

    build: upgrade eslint deps

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -54,6 +54,7 @@ async function cli(args: string[]): Promise<void> {
 
   if (!VALID_COMMANDS.includes(command)) command = 'help'
 
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   const { run, help }: { run: CliCommand; help: CliCommand } = require(`./${command.replace(/:/g, '/')}`)
 
   const cmd = isHelp && command !== 'help' ? help : run
