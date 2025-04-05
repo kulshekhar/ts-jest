70df840b79d4ca36fb071e3d688c639de722f7e0
ahnpnl <anhpnnd@gmail.com>
2025-04-05 11:22:36 +0200

    test: use `ts` Jest config files for e2e tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,22 +1,3 @@
-// /** @type {import('./dist').JestConfigWithTsJest} */
-// module.exports = {
-//   collectCoverageFrom: ['src/**/*.ts'],
-//   modulePathIgnorePatterns: ['examples/.*', 'website/.*'],
-//   setupFilesAfterEnv: ['<rootDir>/src/__helpers__/setup-jest.ts'],
-//   snapshotSerializers: [require.resolve('jest-snapshot-serializer-raw')],
-//   testPathIgnorePatterns: ['src/__mocks__/*', '/node_modules/', '/examples/', '/e2e/.*/__tests__', '\\.snap$'],
-//   transform: {
-//     '^.+.tsx?$': [
-//       '<rootDir>/legacy.js',
-//       {
-//         tsconfig: 'tsconfig.spec.json',
-//         isolatedModules: true,
-//       },
-//     ],
-//   },
-//   coverageProvider: 'v8',
-// }
-
 import { type JestConfigWithTsJest, TS_TRANSFORM_PATTERN } from './src'
 import './scripts/create-bundle.js'
 
