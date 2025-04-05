5b69bc2744c119b785b1d8382376ed0a14559138
ahnpnl <anhpnnd@gmail.com>
2025-04-05 11:30:22 +0200

    refactor: remove unused codes in `json.ts`

--- src/utils/json.ts
+++ src/utils/json.ts
@@ -10,32 +10,3 @@ export function stringify(input: unknown): string {
 export function parse(input: string): any {
   return input === UNDEFINED ? undefined : JSON.parse(input)
 }
-
-interface NormalizeOptions {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  parse?: (input: string) => any
-}
-/**
- * @internal
- */
-export function normalize(input: string, { parse: parser = parse }: NormalizeOptions = {}): string {
-  let result: string | undefined
-  if (normalize.cache.has(input)) {
-    result = normalize.cache.get(input)
-  } else {
-    const data = parser(input)
-    result = stringify(data)
-    if (result === input) result = undefined
-    normalize.cache.set(input, result)
-  }
-
-  return result ?? input
-}
-
-/**
- * @internal
- */
-// eslint-disable-next-line @typescript-eslint/no-namespace
-export namespace normalize {
-  export const cache = new Map<string, string | undefined>()
-}
