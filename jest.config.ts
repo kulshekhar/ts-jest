c86875896b706530393af65200fced989b087453
ahnpnl <anhpnnd@gmail.com>
2025-04-15 19:43:23 +0200

    build: update tsconfig to fix errors with `commitlint.config.ts`

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,10 @@
-import { type JestConfigWithTsJest, TS_TRANSFORM_PATTERN } from './src'
+import type { Config } from 'jest'
+
+import { TS_TRANSFORM_PATTERN } from './src'
+
 import './scripts/create-bundle.js'
 
-const config: JestConfigWithTsJest = {
+export default {
   collectCoverageFrom: ['src/**/*.ts'],
   setupFilesAfterEnv: ['<rootDir>/src/__helpers__/setup-jest.ts'],
   testMatch: ['<rootDir>/src/**/*.spec.ts'],
@@ -12,11 +15,11 @@ const config: JestConfigWithTsJest = {
         tsconfig: {
           target: 'ES2015',
           isolatedModules: true,
+          module: 'NodeNext',
+          moduleResolution: 'NodeNext',
         },
       },
     ],
   },
   coverageProvider: 'v8',
-}
-
-export default config
+} satisfies Config
