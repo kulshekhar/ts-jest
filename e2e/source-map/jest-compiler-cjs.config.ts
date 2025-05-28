c81b6675218ab9d0f9cdd09554ad183706051f8a
ahnpnl <anhpnnd@gmail.com>
2025-05-28 03:25:15 +0200

    feat: support Jest 30

--- e2e/source-map/jest-compiler-cjs.config.ts
+++ e2e/source-map/jest-compiler-cjs.config.ts
@@ -1,13 +1,14 @@
-import { type JestConfigWithTsJest, TS_TRANSFORM_PATTERN } from 'ts-jest'
+import { type JestConfigWithTsJest, TS_JS_TRANSFORM_PATTERN } from 'ts-jest'
 
 export default {
   displayName: 'source-map-compiler-cjs',
   transform: {
-    [TS_TRANSFORM_PATTERN]: [
+    [TS_JS_TRANSFORM_PATTERN]: [
       'ts-jest',
       {
         tsconfig: '<rootDir>/../tsconfig-cjs.spec.json',
       },
     ],
   },
+  transformIgnorePatterns: ['<rootDir>/../node_modules/(?!strip-ansi)'],
 } satisfies JestConfigWithTsJest
